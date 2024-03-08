87a39db5b4b134aeef80dff51e9fa25e7875fa90
Mubarek Shikur <manmuba90@gmail.com>
2024-03-08 10:16:25 +0300

    Done

--- api/settings.py
+++ api/settings.py
@@ -60,14 +60,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    # Added here
     "https://opmsurvey.pythonanywhere.com",
     "http://opmsurvey.pythonanywhere.com",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://esraelsurvey.pythonanywhere.com",
 ]
 
 
