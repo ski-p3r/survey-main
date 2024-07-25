9c2a7e021fd82d9fa65e630c5a835090130c6e43
Mubarek Shikur <manmuba90@gmail.com>
2024-07-25 19:37:43 +0300

    done

--- api/settings.py
+++ api/settings.py
@@ -59,19 +59,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# CORS_ALLOWED_ORIGINS = [
-#     # Added here
-#     "https://opm3.pythonanywhere.com/",
-#     "http://opm3.pythonanywhere.com/",
-#     # "https://opmsurvey.pythonanywhere.com",
-#     # "http://opmsurvey.pythonanywhere.com",
-#     # "http://127.0.0.1:8000",
-#     # "http://localhost:8000",
-#     # "http://127.0.0.1:5173",
-#     # "http://localhost:5173",
-#     # "https://esraelsurvey.pythonanywhere.com",
-# ]
+
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+]
+
 
 ROOT_URLCONF = 'api.urls'
 
